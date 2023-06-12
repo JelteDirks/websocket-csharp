a19eb63250d28d75c1e847d303c9f13fbc5a2e32
Jelte Dirks <j.dirks@outlook.com>
2023-06-12 12:14:25 +0200

    send welcome message and use client structure to track clients
    Signed-off-by: Jelte Dirks <j.dirks@outlook.com>

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using WebSocketExample;
+﻿using WebSocket;
 
 
 
--- WebSocketServer/WebSocketServer.cs
+++ WebSocketServer/WebSocketServer.cs
@@ -1,45 +1,46 @@
 using System.Net;
 using System.Net.WebSockets;
 
-namespace WebSocketExample
+namespace WebSocket
 {
     public class WebSocketServer
     {
-        private HttpListener _listener;
-        private List<WebSocket> _clients;
+        private string welcome_message = "Welcome... to Locus!";
+        private HttpListener http_listener;
+        private List<Client> clients;
 
         public WebSocketServer(string uriPrefix)
         {
-            _listener = new HttpListener();
-            _listener.Prefixes.Add(uriPrefix);
-            _clients = new List<WebSocket>();
+            http_listener = new HttpListener();
+            http_listener.Prefixes.Add(uriPrefix);
+            clients = new List<Client>();
         }
-
 
         public async Task BroadcastAsync(string message)
         {
             var buffer = System.Text.Encoding.UTF8.GetBytes(message);
-            foreach (var client in _clients)
+            foreach (var client in clients)
             {
-                if (client.State == WebSocketState.Open)
+                if (client.WebSocket.State == WebSocketState.Open)
                 {
-                    await client.SendAsync(new ArraySegment<byte>(buffer), WebSocketMessageType.Text, true, CancellationToken.None);
+                    await client.WebSocket.SendAsync(new ArraySegment<byte>(buffer), WebSocketMessageType.Text, true, CancellationToken.None);
                 }
             }
         }
 
         public async Task StartAsync()
         {
-            _listener.Start();
+            http_listener.Start();
             while (true)
             {
-                var context = await _listener.GetContextAsync();
+                var context = await http_listener.GetContextAsync();
                 if (context.Request.IsWebSocketRequest)
                 {
                     // Add your own logic here before accepting the connection
                     var wsContext = await context.AcceptWebSocketAsync(null);
                     var webSocket = wsContext.WebSocket;
-                    _clients.Add(webSocket);
+                    var client = new Client(webSocket, true);
+                    clients.Add(client);
                     _ = HandleConnectionAsync(webSocket);
                 }
                 else
@@ -50,28 +51,49 @@ namespace WebSocketExample
             }
         }
 
-        private async Task HandleConnectionAsync(WebSocket webSocket)
+        private async Task HandleConnectionAsync(System.Net.WebSockets.WebSocket webSocket)
         {
             var buffer = new byte[1024 * 4];
+            // Send welcome message to client
+            await webSocket.SendAsync(new ArraySegment<byte>(System.Text.Encoding.UTF8.GetBytes(welcome_message)), WebSocketMessageType.Text, true, CancellationToken.None);
+
+            var client = clients.Find(c => c.WebSocket == webSocket);
+
             while (webSocket.State == WebSocketState.Open)
             {
                 var result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
                 if (result.MessageType == WebSocketMessageType.Close)
                 {
                     await webSocket.CloseAsync(WebSocketCloseStatus.NormalClosure, string.Empty, CancellationToken.None);
+                    return;
                 }
                 else
                 {
-                    foreach (var client in _clients)
+                    if (client!.Validated == false)
                     {
-                        if (client.State == WebSocketState.Open)
-                        {
-                            await client.SendAsync(new ArraySegment<byte>(buffer, 0, result.Count), result.MessageType, result.EndOfMessage, CancellationToken.None);
-                        }
+                        client.Validated = true;
+                        // Perform session ID validation here
+                        // ...
                     }
+
+                    await webSocket.SendAsync(new ArraySegment<byte>(System.Text.Encoding.UTF8.GetBytes("ooh that tickles")),
+                            WebSocketMessageType.Text, true, CancellationToken.None);
                 }
             }
-            _clients.Remove(webSocket);
+            clients.Remove(client!);
         }
     }
+
+    public class Client
+    {
+        public System.Net.WebSockets.WebSocket WebSocket;
+        public bool Validated;
+
+        public Client(System.Net.WebSockets.WebSocket ws, bool b)
+        {
+            WebSocket = ws;
+            Validated = b;
+        }
+    }
+
 }
